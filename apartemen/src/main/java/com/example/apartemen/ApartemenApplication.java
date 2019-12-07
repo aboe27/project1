2d9c3693597143e2cc489a181e2ea9a3ed89e233
btpnsshifted <btpnsshifted@H0001N593605D8.corp.btpns.com>
2019-12-07 12:23:24 +0700

    menambah file

---
+++ apartemen/src/main/java/com/example/apartemen/ApartemenApplication.java
@@ -0,0 +1,13 @@
+package com.example.apartemen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartemenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartemenApplication.class, args);
+	}
+
+}
---
+++ apartemen/src/test/java/com/example/apartemen/ApartemenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apartemen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartemenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
